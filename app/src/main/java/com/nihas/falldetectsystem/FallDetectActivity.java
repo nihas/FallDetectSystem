fced7641d14c846bb71d508ccf1229ee5dac202f
Nihas <nihasnizar@gmail.com>
2016-01-06 18:49:49 +0530

    meee

---
+++ app/src/main/java/com/nihas/falldetectsystem/FallDetectActivity.java
@@ -0,0 +1,16 @@
+package com.nihas.falldetectsystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by snyxius on 6/1/16.
+ */
+public class FallDetectActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
